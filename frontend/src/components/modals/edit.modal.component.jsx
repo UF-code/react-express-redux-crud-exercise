dcc6b6752e903fa2d16c788ea60a4683ca668775
uf-code <ugur.firat.developer@gmail.com>
2022-06-28 21:32:01 +0300

    dealing with modals

--- frontend/src/components/modals/edit.modal.component.jsx
+++ frontend/src/components/modals/edit.modal.component.jsx
@@ -8,13 +8,34 @@ const EditModal = (props) => {
 
     return (
         <Modal show={props.show} size='md' popup={true} onClose={props.onClose}>
-            {/* <Modal show={true} size='md' popup={true}> */}
             <Modal.Header />
             <Modal.Body>
                 <div className='space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8'>
                     <h3 className='text-xl font-medium text-gray-900 dark:text-white'>
                         Sign in to our platform
                     </h3>
+                    <div>
+                        <div className='mb-2 block'>
+                            <Label htmlFor='first_name' value='First Name' />
+                        </div>
+                        <TextInput
+                            id='first_name'
+                            className='dark:border-gray-500 dark:bg-gray-600'
+                            placeholder='First Name'
+                            required={true}
+                        />
+                    </div>
+                    <div>
+                        <div className='mb-2 block'>
+                            <Label htmlFor='last_name' value='Last Name' />
+                        </div>
+                        <TextInput
+                            id='last_name'
+                            className='dark:border-gray-500 dark:bg-gray-600'
+                            placeholder='Last Name'
+                            required={true}
+                        />
+                    </div>
                     <div>
                         <div className='mb-2 block'>
                             <Label htmlFor='email' value='Your email' />
@@ -26,40 +47,18 @@ const EditModal = (props) => {
                             required={true}
                         />
                     </div>
-                    <div>
-                        <div className='mb-2 block'>
-                            <Label htmlFor='password' value='Your password' />
-                        </div>
-                        <TextInput
-                            id='password'
-                            className='dark:border-gray-500 dark:bg-gray-600'
-                            type='password'
-                            required={true}
-                        />
-                    </div>
-                    <div className='flex justify-between'>
-                        <div className='flex items-center gap-2'>
-                            <Checkbox id='remember' />
-                            <Label htmlFor='remember'>Remember me</Label>
-                        </div>
-                        <a
-                            href='/modal'
-                            className='text-sm text-blue-700 hover:underline dark:text-blue-500'
-                        >
-                            Lost Password?
-                        </a>
-                    </div>
-                    <div className='w-full'>
-                        <Button>Log in to your account</Button>
-                    </div>
-                    <div className='text-sm font-medium text-gray-500 dark:text-gray-300'>
-                        Not registered?{' '}
-                        <a
-                            href='/modal'
-                            className='text-blue-700 hover:underline dark:text-blue-500'
-                        >
-                            Create account
-                        </a>
+
+                    <div className='flex flex-wrap items-center gap-2'>
+                        <button class='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800'>
+                            <span class='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                                Edit Customer
+                            </span>
+                        </button>
+                        <button class='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800'>
+                            <span class='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                                Cancel Edit
+                            </span>
+                        </button>
                     </div>
                 </div>
             </Modal.Body>
--- frontend/src/components/table.component.jsx
+++ frontend/src/components/table.component.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import EditModal from './modals/edit.modal.component'
-import { Button } from 'flowbite-react'
+import { Button, Avatar } from 'flowbite-react'
 
 import { useSelector, useDispatch } from 'react-redux'
 import { addCustomer, deleteCustomer } from '../redux/customerSlice'
@@ -10,18 +10,20 @@ const TableComponent = () => {
     const dispatch = useDispatch()
     const customers = useSelector((state) => state.customers.customers)
 
-    const [show, setShow] = useState(false)
+    const [showAddModal, setShowAddModal] = useState(false)
+    const [showEditModal, setShowEditModal] = useState(false)
+    const [showDeleteModal, setShowDeleteModal] = useState(false)
 
     return (
         <div className=''>
             <div className='flex items-center justify-end p-4'>
                 <button
                     type='button'
-                    class='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'
+                    className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800'
                     onClick={() => {
                         dispatch(
                             addCustomer({
-                                id: 2,
+                                id: 26,
                                 first_name: 'ugur1',
                                 last_name: 'firat1',
                                 email: 'ugur@firat1.com',
@@ -32,7 +34,9 @@ const TableComponent = () => {
                         )
                     }}
                 >
-                    Add New Customer
+                    <span class='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                        Add New Customer
+                    </span>
                 </button>
             </div>
 
@@ -40,6 +44,9 @@ const TableComponent = () => {
                 <table class='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
                     <thead class='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
                         <tr>
+                            <th scope='col' class='px-6 py-3'>
+                                Avatar
+                            </th>
                             <th scope='col' class='px-6 py-3'>
                                 ID
                             </th>
@@ -72,6 +79,15 @@ const TableComponent = () => {
                     <tbody>
                         {customers.map((customer) => (
                             <tr class='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
+                                <td class='px-6 py-4'>
+                                    <img
+                                        class='w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500'
+                                        src={`https://flowbite.com/docs/images/people/profile-picture-${
+                                            (customer.id % 5) + 1
+                                        }.jpg`}
+                                        alt='Bordered avatar'
+                                    />
+                                </td>
                                 <th
                                     scope='row'
                                     class='px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap'
@@ -98,12 +114,15 @@ const TableComponent = () => {
                                     <Button
                                         class='font-medium text-blue-600 dark:text-blue-500 hover:underline px-0'
                                         onClick={() => {
-                                            setShow(true)
+                                            setShowEditModal(true)
                                         }}
                                     >
                                         Edit
                                     </Button>
-                                    <EditModal onClose={() => setShow(false)} show={show} />
+                                    <EditModal
+                                        onClose={() => setShowEditModal(false)}
+                                        show={showEditModal}
+                                    />
                                 </td>
                                 <td class='px-6 py-4 '>
                                     <button
